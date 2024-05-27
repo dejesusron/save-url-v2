475489af8039d79b54c8d1da34d810b72be136d6
lhc-dejesusron <r.dejesus@lionheart.asia>
2024-05-27 13:22:57 +0800

    set up dist folder for client

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ import linkRoute from './routes/linkRoute.js';
 import errorHandler from './middlewares/errorMiddleware.js';
 import connectDB from './configs/db.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// resolving dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
 
 dotenv.config();
 connectDB();
@@ -18,6 +26,14 @@ app.use(cors());
 app.use('/api/links', linkRoute);
 app.use('/api/users', userRoute);
 app.use(errorHandler);
+
+// use the client app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//render the client for any path
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+);
 
 app.listen(port, (req, res) => {
   console.log(`Server is running on port ${port}`);
